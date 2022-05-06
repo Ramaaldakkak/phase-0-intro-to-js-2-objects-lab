e2874f357213864647eb594889e0176a744a3bb2
Rama Aldakkak <rama.aldakkak@gmail.com>
2022-05-06 07:24:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+let employee = {
+    name: "Walt Disney",
+    streetAddress: "Disney"
+}
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const updateEmployee = {...employee};
+    updateEmployee[key] = value;
+    return updateEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey (employee, key) {
+    const updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
